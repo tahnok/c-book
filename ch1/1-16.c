065cf27f75ec4e3b9137c1a2ba79d4cd7b3c5e64
Wesley Ellis <tahnok@gmail.com>
2014-12-09 15:18:15 -0500

    1.16

---
+++ ch1/1-16.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <limits.h>
+
+int getline2(char line[], int maxline);
+void copy(char to[], char from[]);
+
+main()
+{
+  unsigned int len;
+  unsigned int max;
+  char line[UINT_MAX];
+  char longest[UINT_MAX];
+
+  max = 0;
+  while ((len = getline2(line, UINT_MAX)) > 0)
+    if (len > max) {
+      max = len;
+      copy(longest, line);
+    }
+  if (max > 0)
+    printf("%s", longest);
+  return 0;
+}
+
+/* getline2: read a line into s, return length */
+int getline2(char s[], int lim)
+{
+  int c, i;
+
+  for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    s[i] = c;
+  if (c == '\n'){
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+void copy(char to[], char from[])
+{
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0')
+    ++i;
+}
