8ac80c0ee03b3be396c962873c9adeb2420fba53
Wesley Ellis <tahnok@gmail.com>
2015-01-19 15:20:11 -0500

    2-6

--- ch2/2-6.c
+++ ch2/2-6.c
@@ -6,30 +6,46 @@ unsigned int getbits(unsigned int x, int p, int n) {
   return (x >> (p+1-n)) & ~(~0 << n);
 }
 
-//Returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged
-unsigned int setbits(unsigned int x, int p, int n, unsigned int y)
-{
-  //will have 0s at position p to p+n and 1s everywhere else
-  unsigned int ones_mask = (~0 << p) | (~0 >> (p+n+1));
-  return x | (~0 << p);
-}
-//setbits(0b001,3,2,0b010) -> 0b101
-//
 unsigned int ones_mask(int p, int n)
 {
   return (~0 << p) | ~(~0 << (p-n));
 }
 
+
+//Returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged
+unsigned int setbits(unsigned int x, int p, int n, unsigned int y)
+{
+  unsigned int mask = x & ones_mask(p,n);
+  unsigned int set = getbits(y, n-1, n) << p;
+  return mask | set;
+}
+//setbits(0b001,3,2,0b010) -> 0b101
+//
+
 void printbits(unsigned int counter)
 {
-  printf("%u: ", counter);
   for(int x = 0; x < 33; x++){
     printf("%u", (counter >> (32 - x)) & 1);
   }
+  printf(": %u", counter);
   printf("\n");
 }
 
 int main()
 {
-  printbits(ones_mask(9,8));
+  unsigned int x = ~0;
+  int p = 5;
+  int n = 3;
+  unsigned int y = ~0;
+
+  printf("x: ");
+  printbits(x);
+  printf("p: ");
+  printbits(p);
+  printf("n: ");
+  printbits(n);
+  printf("y: ");
+  printbits(y);
+  printf("=  ");
+  printbits(setbits(x,p,n,y));
 }
