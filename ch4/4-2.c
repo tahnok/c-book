9e832db42461b8ac46b0aed62943e5b543ffdeb8
Wesley Ellis <tahnok@gmail.com>
2015-02-23 16:22:47 -0500

    4-2

--- ch4/4-2.c
+++ ch4/4-2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #include <ctype.h>
 /* atof: convert string s to double */
@@ -25,11 +26,21 @@ double atof(char s[])
     i++;
   for (exp = 0.0; isdigit(s[i]); i++)
     exp = 10.0 * exp + (s[i] - '0');
-
-  return sign * val / power;
+  return sign * val / power * pow(10.0, exp_sign * exp);
 }
 
 int main()
 {
+  char test1[] = "12.34E-2";
+  printf("%s: %f\n", test1,  atof(test1));
 
+  char test2[] = "12.34E2";
+  printf("%s: %f\n", test2, atof(test2));
+
+  char test3[] = "12.34";
+  printf("%s: %f\n", test3, atof(test3));
+
+
+  char test4[] = "12.34e+10";
+  printf("%s: %f\n", test4, atof(test4));
 }
