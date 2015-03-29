bbe1b74eff43f3e66f4cf4001b422c1d2d1e91b5
Wesley Ellis <tahnok@gmail.com>
2015-03-29 15:10:14 -0400

    5-1, WIP 5-2

--- ch5/5-2.c
+++ ch5/5-2.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define MAX 10
+
 int getch(void);
 void ungetch(int);
 /* getint: get next integer from input into *pn */
 
-int getint(int *pn)
+int getflot(float *pn)
 {
   int c, sign, i;
+  float fractional = 0.0;
   while (isspace(c = getch())) /* skip white space */ ;
   if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
     ungetch(c); /* it is not a number */
@@ -19,12 +22,38 @@ int getint(int *pn)
   for(i = 0, *pn = 0; isdigit(c); i++, c = getch()) {
     *pn = 10 * *pn + (c - '0');
   }
-  if (i ==0 && *pn == 0) {
+  if (i == 0 && *pn == 0) {
     ungetch(c);
     return 0;
   }
+  if (c == '.') {
+    for(i = 0, fractional = 0.0; isdigit(c); i++, c = getch()) {
+      fractional = 10 * fractional + (c - '0');
+    }
+    *pn = *pn + (fractional / (i * 10.0));
+  }
+
   *pn *= sign;
   if (c != EOF)
     ungetch(c);
   return c;
+}
+
+int main()
+{
+  char[] = 
+
+
+int bufp = 0;
+int buf[MAX];
+
+int getch(void)
+{
+    return bufp > 0 ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp < MAX)
+          buf[bufp++] = c;
 }
