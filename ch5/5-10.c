78de2e496854aa97f5049902d43cf84c8b5e3b67
Wesley Ellis <tahnok@gmail.com>
2015-04-22 12:47:35 -0400

    WIP 5-10

--- ch5/5-10.c
+++ ch5/5-10.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define STACK_SIZE 100
+#define ERROR -1
+
+static int stack[STACK_SIZE];
+int stack_pointer;
+
+int push(int);
+
+int main(int argc, char *argv[])
 {
-
-  char *mystring;
-  
-  char *read = gets(mystring);
+  stack_pointer = 0;
+  printf("got %d args\n", argc);
+  --argc;
+  ++argv;
+  while(--argc > 0) {
+    push(atoi(*++argv));
+  }
+  for(int i = 0; i < stack_pointer; i++) {
+    printf("%d: %d\n", i, stack[i]);
+  }
 }
 
-void expr() {
-
+int push(int n)
+{
+  if (stack_pointer >= STACK_SIZE) {
+    return ERROR;
+  }
+  stack[stack_pointer] = n;
+  stack_pointer++;
+  return 0;
 }
+
