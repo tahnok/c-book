a39e80c3c1ebacad088c627d29b4eb5ac729b306
Wesley Ellis <tahnok@gmail.com>
2015-05-07 19:15:24 -0400

    5-13

--- ch5/5-13.c
+++ ch5/5-13.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void error(char *msg) {
+  printf("%s\n", msg);
+  exit(-1);
+}
+
 int main(int argc, char **argv)
 {
   int num_lines;
@@ -8,26 +13,26 @@ int main(int argc, char **argv)
     num_lines = 10;
   } else if (argc == 2) {
     argv++;
+    if (**argv != '-') {
+      error("invalid format: use -n where n is number of lines to show");
+    }
+    (*argv)++;
     num_lines = atoi(*argv);
     if (num_lines < 1) {
-      printf("must provide a positive number of lines to buffer\n");
-      exit(1);
+      error("must provide a positive number of lines to buffer\n");
     }
   } else {
-    printf("DERP\n");
-    exit(1);
+    error("DERP\n");
   }
 
   char lines[num_lines][100];
 
   int counter = 0;
   while(fgets(lines[counter], 100, stdin) != NULL) {
-    counter = counter + 1 % num_lines;
+    counter = (counter + 1) % num_lines;
   }
-  int old_counter = counter;
-  counter++;
-  while(old_counter != counter) {
-    printf("%s\n", lines[counter]);
-    counter = counter + 1 % num_lines;
+  for(int i; i < num_lines; i++) {
+    printf("%s", lines[counter]);
+    counter = (counter + 1) % num_lines;
   }
 }
